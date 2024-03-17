06d5f19a01e94824fee23c386ed877b00e26fc6e
Victor Chaplygin <guyswinter0@gmail.com>
2024-03-18 01:14:39 +0300

    Добавил кастомный шрифт

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'Hero',
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
